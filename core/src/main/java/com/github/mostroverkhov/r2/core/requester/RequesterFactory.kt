188c5fa0549abb491f7442ef718794b079ee0206
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2018-01-01 11:21:11 +0200

    Requester: in case of resolving error,signal it instead of throwing if return type is Publisher (#17)

--- core/src/main/java/com/github/mostroverkhov/r2/core/requester/RequesterFactory.kt
+++ core/src/main/java/com/github/mostroverkhov/r2/core/requester/RequesterFactory.kt
@@ -1,6 +1,5 @@
 package com.github.mostroverkhov.r2.core.requester
 
-import com.github.mostroverkhov.r2.core.internal.requester.Call
 import com.github.mostroverkhov.r2.core.internal.requester.CallAdapter
 import com.github.mostroverkhov.r2.core.internal.requester.RequesterCallResolver
 import com.github.mostroverkhov.r2.core.internal.requester.TargetAction
@@ -12,24 +11,28 @@ import java.lang.reflect.Proxy
 class RequesterFactory internal constructor(private val adapter: CallAdapter,
                                             private val callResolver: RequesterCallResolver) {
 
-    fun <T> create(clazz: Class<T>): T = CallHandler(callResolver, clazz).handleWith { adapter.adapt(it) }
+    fun <T> create(clazz: Class<T>): T = CallHandler(callResolver, clazz).handleWith(adapter)
 
     /*introduced for testing*/
     internal class CallHandler<T>(private val callResolver: RequesterCallResolver,
                                   private val clazz: Class<T>) {
         @Suppress("UNCHECKED_CAST")
-        fun handleWith(callHandler: (Call) -> Any): T = Proxy.newProxyInstance(
+        fun handleWith(callAdapter: CallAdapter): T = Proxy.newProxyInstance(
                 CallHandler::class.java.classLoader,
                 arrayOf(clazz),
-                Handler(callHandler)) as T
+                Handler(callAdapter)) as T
 
-        private inner class Handler(private val callAdapter: (Call) -> Any) : InvocationHandler {
+        private inner class Handler(private val callAdapter: CallAdapter) : InvocationHandler {
 
             override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any {
                 return if (method.declaringClass === Any::class.java)
                     method.invoke(proxy, args)
                 else {
-                    callAdapter(callResolver.resolve(TargetAction(proxy, method, args)))
+                    try {
+                        callAdapter.adapt(callResolver.resolve(TargetAction(proxy, method, args)))
+                    } catch (e: RuntimeException) {
+                        callAdapter.resolve(method, e)
+                    }
                 }
             }
         }
